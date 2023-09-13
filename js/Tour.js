38ca300d14c9f4d633ea9794370a232fa590258b
rafsywhj <70297474+rafsywhj@users.noreply.github.com>
2023-09-14 00:23:43 +0400

    Update Tour.js

--- js/Tour.js
+++ js/Tour.js
@@ -24,27 +24,23 @@ AFRAME.registerComponent("tour", {
   },
   createPlace: function() {
     const details = {
-      garden: {
-        position: { x: 20, y: -4.5, z: -5.5 },
-        rotation: { x: 0, y: -90, z: 0 },
-        src: "./assets/thumbnails/garden.png",
-        title: "Garden",
-        id: "garden"
-      },
-      main_gate: {
-        position: { x: 4.6, y: -5.5, z: 25 },
-        rotation: { x: 180, y: 0, z: 0 },
-        src: "./assets/thumbnails/main_gate.png",
-        title: "Main Gate",
-        id: "main_gate"
-      },
-      home: {
-        position: { x: -9, y: 34, z: -100 },
-        rotation: { x: 0, y: 0, z: 0 },
-        src: "./assets/thumbnails/home.png",
-        title: "My Home",
-        id: "home"
-      }
+      // write code here..............
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
+     
     };
 
     for (var key in details) {
@@ -60,17 +56,17 @@ AFRAME.registerComponent("tour", {
 
   createThumbNail: function(item) {
     const entityEl = document.createElement("a-entity");
-    const id = `place-${item.id}`;
-    entityEl.setAttribute("visible", true);
-    entityEl.setAttribute("id", id);
-    entityEl.setAttribute("geometry", {
-      primitive: "circle",
-      radius: 3
-    });
-    entityEl.setAttribute("position", item.position);
-    entityEl.setAttribute("rotation", item.rotation);
-    entityEl.setAttribute("material", { src: item.src, opacity: 0.6 });
-    entityEl.setAttribute("cursor-listener", {});
+
+// write code here.........
+
+
+
+
+
+
+
+
+    
     return entityEl;
   },
   createTitleEl: function(item) {
